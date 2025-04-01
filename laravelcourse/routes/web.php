e7fe806b072ba42272b90c713749f4658e56467f
mlondono13 <mlondonol@eafit.edu.co>
2025-03-31 20:04:17 -0500

    Tutorial 4 implementado

--- laravelcourse/routes/web.php
+++ laravelcourse/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 Route::get('/about', function () {
